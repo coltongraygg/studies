1fa6d09fc208e3462dbd9a5ade8f5f416f2eb066
coltongraygg <163790834+coltongraygg@users.noreply.github.com>
2024-08-07 20:05:59 +0000

    Added forEach, map, and filter review file

---
+++ Presentations/operators.js
@@ -0,0 +1,96 @@
+// // proper format of if else if else conditional statmeent
+
+// var color = 'red';
+// if(color === 'purple') {
+//     console.log('the color is purple');
+// } if (color === 'red') {
+//     console.log('the color is red');
+// } else {
+//     console.log('the color is unknown')
+// }
+
+// var color = 'red';
+// if (color === 'purple') {
+//     console.log('purple');
+// } else if (color === 'red') {
+//     console.log('colos is red');
+// } else {
+//     console.log('unknown color');
+// }
+
+// var = red;
+// if(color === 'purple')
+// {
+//     console.log('the color is purple!');
+// }
+// else if(color === 'red');
+// {
+//     console.log('red');
+// }
+// else 
+// {
+//     console.log('unkown');
+// }
+
+// i chose the second
+
+
+// var color = 'purple';
+// switch(color) {
+//     case 'green':
+//         console.log('green');
+//         break;
+//     case 'yellow':
+//         console.log('yellow');
+//         break;
+//     case 'red':
+//         console.log('red');
+//         break;
+//     default:
+//         console.log('sorry');
+// }
+
+
+// if (1 + 1 > 2 + 2) {
+//     console.log('first sum is greater');
+// } else {
+//     console.log('second is greater');
+// }
+
+// Conditional statements rely on an evaluation of what?
+    // A. Evaluations in a numeric context by coercing values to 1 or 0
+    // B. The evaluation of positive numeric values.
+    // C. Evaluations in a boolean context by coercing values to true or false
+    // D. The evaluation of null or undefined
+// C
+
+
+// var color = 'red';
+// switch (color) {
+//     case 'green':
+//         console.log('green');
+//         break;
+//     case 'yellow':
+//         console.log('yellow');
+//         break;
+//     case 'red':
+//         console.log('red');
+//         break;
+//     default:
+//         console.log('sorry');
+// }
+// // red
+
+// Conditional statements allow us to do what?
+    // A. Execute code blocks based on whether a condition is true or false
+    // B. Change the condition of variables
+    // C. Create new application states
+    // D. Create new objects
+// A
+
+var users = [];
+if(users.length) {
+    console.log('there are ' + users.length + ' in the app');
+} else {
+    console.log('no users');
+}
